af60121ab46ad04c4a58f9b6ceddc12bd96b7c56
Federico <federico@federico-MS-7369.(none)>
2013-06-03 17:47:40 -0300

    tests con Encripcion y desencripcion

---
+++ testEncrypt.c
@@ -0,0 +1,179 @@
+#include "includes/encrypt.h"
+#include "includes/decrypt.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int 
+main(void){
+	unsigned char * message;
+        unsigned char * enc;
+        unsigned char * out;
+        unsigned char * iv;
+        unsigned char * password;
+       
+        password = calloc(9,1);
+        memcpy((char *)password, "dinudinu",8);
+       
+        iv = calloc(9,1);
+        memcpy((char *)iv, "holacomo",8);
+       
+        message = calloc(100,1);
+        strcpy((char *)message, "mensajede16bitss");
+       
+        int length = strlen((char *)message);
+	printf("El Mensaje a encryptar es :%s\n",message);
+	printf("PRUEBA DE DES OFB\n");        
+        enc = mydes_ofb_encrypt( message, length, password, iv);
+        out = mydes_ofb_decrypt( enc, length, password, iv);
+	printf("%s\n",message);
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE DES ECB\n"); 
+	enc=mydes_ecb_encrypt( message, length, password);
+	out=mydes_ecb_decrypt( enc, length, password);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE DES CBC\n"); 
+	enc=mydes_cbc_encrypt( message, length, password,iv);
+	out=mydes_cbc_decrypt( enc, length, password,iv);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+
+	printf("PRUEBA DE DES CFB\n"); 
+	enc=mydes_cfb_encrypt( message, length, password,iv);
+	out=mydes_cfb_decrypt( enc, length, password,iv);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+
+	printf("PRUEBA DE AES128 ECB\n"); 
+	enc=myaes_ecb_encrypt( message, length, password,128);
+	out=myaes_ecb_decrypt( enc, length, password,128);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES128 CBC\n"); 
+	enc=myaes_cbc_encrypt( message, length, password,iv,128);
+	out=myaes_cbc_decrypt( enc, length, password,iv,128);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES128 CFB\n"); 
+	enc=myaes_cfb_encrypt( message, length, password,iv,128);
+	out=myaes_cfb_decrypt( enc, length, password,iv,128);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	printf("-----------------------------\n");
+	free(enc);
+	free(out);
+	printf("PRUEBA DE AES128 OFB\n"); 
+	enc=myaes_ofb_encrypt( message, length, password,iv,128);
+	out=myaes_ofb_decrypt( enc, length, password,iv,128);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES 192 ECB\n"); 
+	enc=myaes_ecb_encrypt( message, length, password,192);
+	out=myaes_ecb_decrypt( enc, length, password,192);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES192 CBC\n"); 
+	enc=myaes_cbc_encrypt( message, length, password,iv,192);
+	out=myaes_cbc_decrypt( enc, length, password,iv,192);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES192 CFB\n"); 
+	enc=myaes_cfb_encrypt( message, length, password,iv,192);
+	out=myaes_cfb_decrypt( enc, length, password,iv,192);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	printf("-----------------------------\n");
+	free(enc);
+	free(out);
+	printf("PRUEBA DE AES192 OFB\n"); 
+	enc=myaes_ofb_encrypt( message, length, password,iv,192);
+	out=myaes_ofb_decrypt( enc, length, password,iv,192);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES 256 ECB\n"); 
+	enc=myaes_ecb_encrypt( message, length, password,256);
+	out=myaes_ecb_decrypt( enc, length, password,256);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES256 CBC\n"); 
+	enc=myaes_cbc_encrypt( message, length, password,iv,256);
+	out=myaes_cbc_decrypt( enc, length, password,iv,256);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+	printf("-----------------------------\n");
+	printf("PRUEBA DE AES256 CFB\n"); 
+	enc=myaes_cfb_encrypt( message, length, password,iv,256);
+	out=myaes_cfb_decrypt( enc, length, password,iv,256);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	printf("-----------------------------\n");
+	free(enc);
+	free(out);
+	printf("PRUEBA DE AES256 OFB\n"); 
+	enc=myaes_ofb_encrypt( message, length, password,iv,256);
+	out=myaes_ofb_decrypt( enc, length, password,iv,256);	
+        printf("%s\n",enc);
+        printf("%s\n",out);
+	
+	free(enc);
+	free(out);
+
+	printf("-----------------------------\n");
+
+
+        return 0;
+
+}
+
+ 
